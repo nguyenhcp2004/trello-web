d9138e3571a80be9ff2eb7387361202e3d226152
Huynh Nguyen <yugivip277@gmail.com>
2023-12-09 11:29:26 +0700

    Mock Sort Data

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -10,6 +10,7 @@ import AvatarGroup from '@mui/material/AvatarGroup'
 import Tooltip from '@mui/material/Tooltip'
 import Button from '@mui/material/Button'
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
+import { capitalizeFirstLetter } from '~/utils/formatters'
 
 const MENU_STYLES = {
   color: 'white',
@@ -25,7 +26,7 @@ const MENU_STYLES = {
   }
 }
 
-const Boardbar = () => {
+const Boardbar = ({ board }) => {
   return (
     <Box sx={{
       width: '100%',
@@ -43,13 +44,13 @@ const Boardbar = () => {
         <Chip
           sx={MENU_STYLES}
           icon={<DashboardIcon />}
-          label="NguyenDev MERN Stack Board"
+          label={board?.title}
           clickable
         />
         <Chip
           sx={MENU_STYLES}
           icon={<VpnLockIcon />}
-          label="Public/Private Workspace"
+          label={capitalizeFirstLetter(board?.type)}
           clickable
         />
         <Chip
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,14 +2,15 @@ import Container from '@mui/material/Container'
 import AppBar from '~/components/AppBar/AppBar'
 import Boardbar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
+import { mockData } from '~/apis/mock-data'
 
 const Board = () => {
   return (
     <div>
       <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
         <AppBar />
-        <Boardbar />
-        <BoardContent />
+        <Boardbar board={mockData?.board} />
+        <BoardContent board={mockData?.board} />
       </Container>
     </div>
   )

f818478befbd0af8480c694da0a4cf9948ed9156
Huynh Nguyen <yugivip277@gmail.com>
2023-12-09 07:45:57 +0700

    Trello Board Content

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -92,7 +92,10 @@ const Boardbar = () => {
               width: 34,
               height: 34,
               fontSize: '16px',
-              border: 'none'
+              border: 'none',
+              color: 'white',
+              cursor: 'pointer',
+              '&:first-of-type': { bgcolor: '#a4b0be' }
             }
           }}
         >
